b050ff49a7b5f9e017468a4949266b90ba3629e0
amir <a.m.asadi2001@gmail.com>
2023-09-23 19:47:51 +0330

    add ant desing to client-app and fix cors plicy in backend app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { List } from "antd";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/Activities").then((res) => {
+      setActivities(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <List
+    itemLayout="horizontal"
+    dataSource={activities}
+    renderItem={(item : any, index) => (
+      <List.Item>
+        <List.Item.Meta
+          title={item.title}
+          description={item.description}
+        />
+      </List.Item>
+    )}
+  />
   );
 }
 
